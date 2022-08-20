29ed128fff9a99a4fded540d98cdb8451383e2d5
Suka SyamalanKothapalli <sk22146@corp.ojas-it.com>
2022-08-20 15:49:25 +0530

    msg

---
+++ concatination.py
@@ -0,0 +1,18 @@
+"""Write a Python program to concatenate all elements in a list into a string """
+"""and return it"""
+
+"""L1=["1","2","3","4"]
+L=L1[0]+L1[1]+L1[2]+L1[3]
+print(L)"""
+
+"""l1=input("values in a list").split(",")
+print(l1)
+print("".join(l1))"""
+
+a=[1,2,[4,[6,9]]]
+replace.a(2)=4
+print(a)
+
+
+
+
